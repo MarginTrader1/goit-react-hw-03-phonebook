519168e5fbacaecfcab41a7b0655590f045f1daf
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-08-20 23:07:07 +0300

    сделал и стилизировал блок контактов

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,16 @@
+import { Li, Button, Ul } from "./Contacts.styled";
+
+export const ContactsList = ({ list }) => {
+  return (
+    <Ul>
+      {list.contacts.map(item => (
+        <Li key={item.id}>
+          <p>
+            {item.name}: {item.number}
+          </p>
+          <Button>Delete</Button>
+        </Li>
+      ))}
+    </Ul>
+  );
+};
